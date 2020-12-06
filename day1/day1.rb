b9a5b9bcaca66d4a88ee95290133633113d2f3c7
Gayoon1234 <67247345+Gayoon1234@users.noreply.github.com>
2020-12-06 15:28:56 +1100

    Added Part 2

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,2 +1,3 @@
 arr = File.open("day1.txt", "r").map(&:to_i)
-arr.each{|x| (puts x*(2020-x);break) if arr.include?((2020-x))}
+arr.each{|x| (puts x*(2020-x);break) if arr.include?((2020-x))} #part 1
+arr.each{|x| arr.each{|y| (puts x*y*(2020-x-y);return) if arr.include?(2020-x-y)}} #part 2
