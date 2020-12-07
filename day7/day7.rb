cca5c5f9a1f8e19dc68aa350b582c6776a7e72d5
Gayoon1234 <67247345+Gayoon1234@users.noreply.github.com>
2020-12-07 19:58:05 +1100

    Update day7.rb

--- day7/day7.rb
+++ day7/day7.rb
@@ -4,8 +4,6 @@ ax = Hash.new
 
 arr.each{|x| x.gsub! " bags " ,',';x.gsub! "contain ","";temp = x.split(','); ax[temp[0]] = temp[1..]}
 
-counter = 0
-
 colors = Array.new
 colors.push("shiny gold")
 length = 0
@@ -15,7 +13,7 @@ until length == colors.length
     ax.each do |x,y|
         y.each{|z| z.gsub! "bags","";z.gsub! "bag", "";z.gsub! ".","";z.gsub! /\d+/ ,"";z.strip!}
 
-        colors.each{|z| (counter += 1;colors.push(x)) if y.include? z}
+        colors.each{|z| colors.push(x) if y.include? z}
         colors = colors.uniq    
     end
 end
