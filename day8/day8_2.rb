110784186e911a33b2e60ea19e238333a01be5c3
Gayoon1234 <67247345+Gayoon1234@users.noreply.github.com>
2020-12-10 15:11:04 +1100

    Update day8_2.rb

--- day8/day8_2.rb
+++ day8/day8_2.rb
@@ -21,19 +21,10 @@ def check_if_recursive(test_array)
 
         case instruction
             when "acc" 
-                if test_array[current_index][4].to_s == "+"
-                    accu += test_array[current_index][5..].to_i
-                else
-                    accu -= test_array[current_index][5..].to_i
-                end
-                
+                accu += test_array[current_index][4..].to_i
                 current_index += 1
             when "jmp"
-                if test_array[current_index][4].to_s == "+"
-                    current_index += test_array[current_index][5..].to_i
-                else
-                    current_index -= test_array[current_index][5..].to_i
-                end
+                current_index += test_array[current_index][4..].to_i
             when "nop"
                 current_index += 1
         end
@@ -51,10 +42,10 @@ arr.each do |line|
     index += 1
 
     next if line[0...3].to_s == "acc"
-           
+
     arr2 = File.open("day8.txt").map(&:chomp)
     arr2[index].sub! line[0...3],replacement[line[0...3]]
 
-    puts "#Part 2: {check_if_recursive(arr2)}" if check_if_recursive(arr2) != -1
+    puts "Part 2: #{check_if_recursive(arr2)}" if check_if_recursive(arr2) != -1
 
 end
