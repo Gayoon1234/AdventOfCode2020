a87516cb373d367aa34cd90b696e0c0d084e9ecc
Gayoon1234 <67247345+Gayoon1234@users.noreply.github.com>
2020-12-12 21:15:14 +1100

    Update day10.rb

--- day10/day10.rb
+++ day10/day10.rb
@@ -21,25 +21,3 @@ while true do
 end
 
 puts ones*threes
-
-
-
-@jolts = File.open("day10.txt").map(&:chomp).map(&:to_i)
-@cache = Hash.new
-
-
-def solve(current_jolt=0)
-    return 1 if current_jolt == @jolts.max
-    if !@cache.has_key? current_jolt
-        n_paths = Array.new
-        n_paths.push(solve(current_jolt+1)) if @jolts.include?(current_jolt + 1)
-        n_paths.push(solve(current_jolt+2)) if @jolts.include?(current_jolt + 2)
-        n_paths.push(solve(current_jolt+3)) if @jolts.include?(current_jolt + 3)
-        @cache[current_jolt] = n_paths.sum
-    end
-
-    return @cache[current_jolt]
-end
-
-puts solve()
-
