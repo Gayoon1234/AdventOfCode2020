0fb621176b1f5bd572f064a63291e524e9a1201e
Gayoon1234 <67247345+Gayoon1234@users.noreply.github.com>
2020-12-12 21:16:30 +1100

    Update day1.js

--- day1/day1.js
+++ day1/day1.js
@@ -1,19 +1,6 @@
-//const { Console } = require('console');
-
 var lines = require('fs').readFileSync("day1.txt", 'utf-8') .split('\n') .filter(Boolean);
 for (let number of lines) 
     if(lines.includes((2020-number).toString())){
     console.log(`Part 1: ${number*(2020-number)}`)
     break
     }
-  
-
-
-/*
-lines.forEach(printLine)
-
-function printLine(number){
-    if(lines.includes((2020-number).toString()))
-    console.log(number*(2020-number))
-}
-*/
